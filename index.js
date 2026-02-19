7df131bff4ce5b1aeddf99e49002721e2dbbf18d
chars <l22390451@chetumal.tecnm.mx>
2026-02-19 17:38:47 -0500

    Cambios subidos por Jareth Quevedo

---
+++ index.js
@@ -0,0 +1,14 @@
+function sumar(a, b) {
+  return a + b;
+}
+
+function main() {
+  const numero1 = 5;
+  const numero2 = 7;
+
+  const resultado = sumar(numero1, numero2);
+  console.log(`El resultado de sumar ${numero1} y ${numero2} es: ${resultado}`);
+}
+
+main();
+
